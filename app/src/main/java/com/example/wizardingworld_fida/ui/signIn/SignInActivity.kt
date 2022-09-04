87743e9fabe23c551c5c11f62b49b45a13b5b3f0
fidamohammed <fidammp@gmail.com>
2022-09-04 10:50:39 +0100

    Favourites Implementation

---
+++ app/src/main/java/com/example/wizardingworld_fida/ui/signIn/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wizardingworld_fida.ui.signIn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wizardingworld_fida.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/example/wizardingworld_fida/ui/signUp/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wizardingworld_fida.ui.signUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wizardingworld_fida.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
